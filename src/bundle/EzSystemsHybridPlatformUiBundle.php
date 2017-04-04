bf6aa821de37b125bb4c2fe7e9e8b6c67275fccd
Bertrand Dunogier <bertrand.dunogier@ez.no>
2017-04-04 08:56:28 +0200

    Bootstrapped the bundle

---
+++ src/bundle/EzSystemsHybridPlatformUiBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace EzSystems\HybridPlatformUiBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EzSystemsHybridPlatformUiBundle extends Bundle
+{
+    public function build(ContainerBuilder $container)
+    {
+    }
+}
