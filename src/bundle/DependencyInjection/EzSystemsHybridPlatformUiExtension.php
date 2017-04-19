ebb18ac9bcee0f49e517fb84548f187b7c223091
Bertrand Dunogier <bertrand.dunogier@ez.no>
2017-04-19 11:40:34 +0200

    Implemented location view
    Includes sub-items and universal discovery widget integrations.

--- src/bundle/DependencyInjection/EzSystemsHybridPlatformUiExtension.php
+++ src/bundle/DependencyInjection/EzSystemsHybridPlatformUiExtension.php
@@ -2,17 +2,20 @@
 
 namespace EzSystems\HybridPlatformUiBundle\DependencyInjection;
 
+use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * This is the class that loads and manages your bundle configuration.
  *
  * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
  */
-class EzSystemsHybridPlatformUiExtension extends Extension
+class EzSystemsHybridPlatformUiExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritdoc}
@@ -25,5 +28,18 @@ class EzSystemsHybridPlatformUiExtension extends Extension
         $loader->load('services.yml');
         $loader->load('toolbars.yml');
         $loader->load('components.yml');
+    }
+
+    public function prepend(ContainerBuilder $container)
+    {
+        $this->prependViewsConfiguration($container);
+    }
+
+    private function prependViewsConfiguration(ContainerBuilder $container)
+    {
+        $configFile = __DIR__ . '/../Resources/config/views.yml';
+        $config = Yaml::parse(file_get_contents($configFile));
+        $container->prependExtensionConfig('ezpublish', $config);
+        $container->addResource(new FileResource($configFile));
     }
 }
