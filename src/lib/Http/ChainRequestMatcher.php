2ec2e2a5d4e7b0098baebacc1c0ab54ca1c9f6cc
Bertrand Dunogier <bertrand.dunogier@ez.no>
2017-05-11 15:31:20 +0200

    Added PhpDoc to ChainRequestMatcher

--- src/lib/Http/ChainRequestMatcher.php
+++ src/lib/Http/ChainRequestMatcher.php
@@ -5,6 +5,9 @@ namespace EzSystems\HybridPlatformUi\Http;
 use Symfony\Component\HttpFoundation\RequestMatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * Matches a request using a set of request matchers
+ */
 class ChainRequestMatcher implements RequestMatcherInterface
 {
     /**
@@ -17,6 +20,13 @@ class ChainRequestMatcher implements RequestMatcherInterface
         $this->requestMatchers = $requestMatchers;
     }
 
+    /**
+     * Matches the request if ALL the chained request matchers match.
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return bool
+     */
     public function matches(Request $request)
     {
         foreach ($this->requestMatchers as $requestMatcher) {
