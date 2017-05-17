470966d70fb647998e2f4541b586b56cbc274d5e
Bertrand Dunogier <bertrand.dunogier@ez.no>
2017-05-17 19:07:14 +0200

    [NavigationHub] Added optional route prefix matching to Route Link

--- src/lib/NavigationHub/Link/Route.php
+++ src/lib/NavigationHub/Link/Route.php
@@ -18,14 +18,24 @@ class Route extends Link
     protected $routeName;
 
     /**
-     * @var string
+     * @var array
      */
     protected $routeParams;
 
-    public function __construct(UrlGeneratorInterface $urlGenerator, $routeName, $routeParams, $name, $zoneIdentifier)
+    public $zone;
+
+    public $name;
+
+    /**
+     * An optional route prefix used to match routes.
+     * @var string
+     */
+    protected $routePrefix;
+
+    public function __construct(UrlGeneratorInterface $urlGenerator, $routeName, $name, $zone, array $routeParams = [])
     {
         $this->urlGenerator = $urlGenerator;
-        $this->zone = $zoneIdentifier;
+        $this->zone = $zone;
         $this->name = $name;
         $this->routeName = $routeName;
         $this->routeParams = $routeParams;
@@ -42,8 +52,8 @@ class Route extends Link
         $routeParams = $request->attributes->get('_routeParams', []);
 
         return (
-            $this->matchRoute($routeName)
-            && $this->matchRouteParams($routeParams)
+            ($this->matchRoute($routeName) && $this->matchRouteParams($routeParams))
+            || $this->matchRoutePrefix($routeName)
         );
     }
 
@@ -63,5 +73,25 @@ class Route extends Link
             }
         }
         return true;
+    }
+
+    /**
+     * Matches $routeName against the configured route prefix, if any.
+     *
+     * @param string $routeName
+     *
+     * @return bool
+     */
+    protected function matchRoutePrefix($routeName)
+    {
+        return $this->routePrefix !== null && strpos($routeName, $this->routePrefix) === 0;
+    }
+
+    /**
+     * @param mixed $routePrefix
+     */
+    public function setRoutePrefix($routePrefix)
+    {
+        $this->routePrefix = $routePrefix;
     }
 }
