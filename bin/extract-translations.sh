bcf7ee04cba353a2e7651fef23b1215f5027a6f0
Bertrand Dunogier <bertrand.dunogier@ez.no>
2017-06-15 21:22:11 +0200

    EZP-27494: Added extra-translations.sh script
    Meant to be executed from ezplatform, inside the vendor/ezsystems/hybrid-platform-ui directory.

---
+++ bin/extract-translations.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+cd ../../..;
+# Extract string for default locale
+echo '# Extracting translations from vendor/ezsystems/hybrid-platform-ui';
+./bin/console translation:extract en -v \
+  --dir=./vendor/ezsystems/hybrid-platform-ui/src \
+  --output-dir=./vendor/ezsystems/hybrid-platform-ui/src/bundle/Resources/translations \
+  --keep
+  "$@"
+
+cd vendor/ezsystems/hybrid-platform-ui;
+
+echo '# Clean file references';
+sed -i "s|>.*/hybrid-platform-ui/|>|g" ./src/bundle/Resources/translations/*.xlf
+
+echo 'Translation extraction done';
