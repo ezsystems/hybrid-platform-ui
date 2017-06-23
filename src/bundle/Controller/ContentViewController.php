593088f58bc8557385232273c2e4674221c6eb5c
Max Baldanza <mbaldanza@inviqa.com>
2017-06-23 14:24:45 +0100

    EZP-27443: Display main content tab

--- src/bundle/Controller/ContentViewController.php
+++ src/bundle/Controller/ContentViewController.php
@@ -14,6 +14,7 @@ use eZ\Publish\API\Repository\Values\Content\VersionInfo;
 use eZ\Publish\Core\MVC\Symfony\Controller\Controller;
 use eZ\Publish\Core\MVC\Symfony\View\ContentView;
 use EzSystems\HybridPlatformUi\Filter\VersionFilter;
+use EzSystems\HybridPlatformUi\Repository\UiFieldGroupService;
 use EzSystems\HybridPlatformUi\Repository\UiLocationService;
 
 class ContentViewController extends Controller
@@ -35,6 +36,17 @@ class ContentViewController extends Controller
         Location::SORT_FIELD_DEPTH => ['key' => 'sort.depth', 'default' => 'Location depth'],
         Location::SORT_FIELD_CLASS_NAME => ['key' => 'sort.content.type.name', 'default' => 'Content type name'],
     ];
+
+    public function contentTabAction(ContentView $view, UiFieldGroupService $fieldGroupService)
+    {
+        $versionInfo = $view->getContent()->getVersionInfo();
+
+        $view->addParameters([
+            'fieldGroups' => $fieldGroupService->loadFieldGroups($versionInfo->getContentInfo()),
+        ]);
+
+        return $view;
+    }
 
     public function detailsTabAction(ContentView $view)
     {
