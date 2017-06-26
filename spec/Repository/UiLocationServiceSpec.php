3b9c6e4980324c3c5b755e79232b0e32c7b4a65e
mcurran-inviqa <mcurran@inviqa.com>
2017-06-26 12:56:12 +0100

    Use inheritance over decoration for UiLocation objects (#36)

--- spec/Repository/UiLocationServiceSpec.php
+++ spec/Repository/UiLocationServiceSpec.php
@@ -9,19 +9,19 @@ namespace spec\EzSystems\HybridPlatformUi\Repository;
 use eZ\Publish\API\Repository\LocationService;
 use eZ\Publish\API\Repository\Values\Content\ContentInfo;
 use eZ\Publish\Core\Repository\Values\Content\Location;
-use EzSystems\HybridPlatformUi\Decorator\LocationDecorator;
 use EzSystems\HybridPlatformUi\Repository\PathService;
+use EzSystems\HybridPlatformUi\Repository\Values\Content\UiLocation;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
-class DecoratedLocationServiceSpec extends ObjectBehavior
+class UiLocationServiceSpec extends ObjectBehavior
 {
     function let(LocationService $locationService, PathService $pathService)
     {
         $this->beConstructedWith($locationService, $pathService);
     }
 
-    function it_loads_and_decorates_locations_with_a_child_count(LocationService $locationService)
+    function it_loads_ui_locations_with_a_child_count(LocationService $locationService)
     {
         $contentInfo = new ContentInfo(['mainLocationId' => 1]);
 
@@ -29,14 +29,12 @@ class DecoratedLocationServiceSpec extends ObjectBehavior
         $locationService->loadLocations($contentInfo)->willReturn([$location]);
         $locationService->getLocationChildCount($location)->willReturn(1);
 
-        $locationDecorator = new LocationDecorator($location);
-        $locationDecorator->childCount = 1;
-        $locationDecorator->main = false;
+        $uiLocation = new UiLocation($location, ['childCount' => 1]);
 
-        $this->loadLocations($contentInfo)->shouldBeLike([$locationDecorator]);
+        $this->loadLocations($contentInfo)->shouldBeLike([$uiLocation]);
     }
 
-    function it_loads_and_decorates_locations_with_a_path_location(LocationService $locationService, PathService $pathService)
+    function it_loads_ui_locations_with_a_path_location(LocationService $locationService, PathService $pathService)
     {
         $contentInfo = new ContentInfo(['mainLocationId' => 1]);
 
@@ -45,15 +43,12 @@ class DecoratedLocationServiceSpec extends ObjectBehavior
         $locationService->getLocationChildCount($location)->willReturn(1);
         $pathService->loadPathLocations(Argument::type(Location::class))->willReturn([$location]);
 
-        $locationDecorator = new LocationDecorator($location);
-        $locationDecorator->childCount = 1;
-        $locationDecorator->pathLocations = [$location];
-        $locationDecorator->main = false;
+        $uiLocation = new UiLocation($location, ['childCount' => 1, 'pathLocations' => [$location]]);
 
-        $this->loadLocations($contentInfo)->shouldBeLike([$locationDecorator]);
+        $this->loadLocations($contentInfo)->shouldBeLike([$uiLocation]);
     }
 
-    function it_loads_and_decorates_locations_with_a_main_flag(LocationService $locationService)
+    function it_loads_ui_locations_with_a_main_flag(LocationService $locationService)
     {
         $contentInfo = new ContentInfo(['mainLocationId' => 1]);
 
@@ -61,11 +56,9 @@ class DecoratedLocationServiceSpec extends ObjectBehavior
         $locationService->loadLocations($contentInfo)->willReturn([$location]);
         $locationService->getLocationChildCount($location)->willReturn(1);
 
-        $locationDecorator = new LocationDecorator($location);
-        $locationDecorator->childCount = 1;
-        $locationDecorator->main = true;
+        $uiLocation = new UiLocation($location, ['childCount' => 1, 'main' => true]);
 
-        $this->loadLocations($contentInfo)->shouldBeLike([$locationDecorator]);
+        $this->loadLocations($contentInfo)->shouldBeLike([$uiLocation]);
     }
 
     function it_puts_the_main_location_first(LocationService $locationService)
@@ -79,14 +72,10 @@ class DecoratedLocationServiceSpec extends ObjectBehavior
         $locationService->getLocationChildCount($location)->willReturn(1);
         $locationService->getLocationChildCount($mainLocation)->willReturn(1);
 
-        $locationDecorator = new LocationDecorator($location);
-        $locationDecorator->childCount = 1;
-        $locationDecorator->main = false;
+        $uiLocation = new UiLocation($location, ['childCount' => 1, 'main' => false]);
 
-        $mainLocationDecorator = new LocationDecorator($mainLocation);
-        $mainLocationDecorator->childCount = 1;
-        $mainLocationDecorator->main = true;
+        $mainLocationDecorator = new UiLocation($mainLocation, ['childCount' => 1, 'main' => true]);
 
-        $this->loadLocations($contentInfo)->shouldBeLike([$mainLocationDecorator, $locationDecorator, $locationDecorator]);
+        $this->loadLocations($contentInfo)->shouldBeLike([$mainLocationDecorator, $uiLocation, $uiLocation]);
     }
 }
---
+++ src/lib/Repository/UiLocationService.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace EzSystems\HybridPlatformUi\Repository;
+
+use eZ\Publish\API\Repository\LocationService;
+use eZ\Publish\API\Repository\Values\Content\ContentInfo;
+use eZ\Publish\API\Repository\Values\Content\Location;
+use EzSystems\HybridPlatformUi\Repository\Values\Content\UiLocation;
+
+/**
+ * Service for loading locations with additional data not provided by the original API.
+ * Returns ui location objects which inherit from location and provide additional properties.
+ */
+class UiLocationService
+{
+    /**
+     * @var LocationService
+     */
+    private $locationService;
+
+    /**
+     * @var PathService
+     */
+    private $pathService;
+
+    public function __construct(LocationService $locationService, PathService $pathService)
+    {
+        $this->locationService = $locationService;
+        $this->pathService = $pathService;
+    }
+
+    /**
+     * Loads ui locations for $contentInfo.
+     * Retrieves and sets child count, path locations and whether location is main on each ui location.
+     * Intended usage is admin only, where we display information that isn't provided by the standard location service.
+     *
+     * @param ContentInfo $contentInfo
+     *
+     * @return UiLocation[]
+     */
+    public function loadLocations(ContentInfo $contentInfo)
+    {
+        $locations = $this->locationService->loadLocations($contentInfo);
+
+        $uiLocations = $this->buildUiLocations($locations, $contentInfo);
+        $uiLocations = $this->prioritizeMainLocation($uiLocations);
+
+        return $uiLocations;
+    }
+
+    private function buildUiLocations(array $locations, ContentInfo $contentInfo)
+    {
+        return array_map(
+            function (Location $location) use ($contentInfo) {
+                $properties = [
+                    'childCount' => $this->locationService->getLocationChildCount($location),
+                    'pathLocations' => $this->pathService->loadPathLocations($location),
+                    'main' => ($location->id === $contentInfo->mainLocationId),
+                ];
+
+                $uiLocation = new UiLocation($location, $properties);
+
+                return $uiLocation;
+            },
+            $locations
+        );
+    }
+
+    private function prioritizeMainLocation(array $locations)
+    {
+        foreach ($locations as $key => $location) {
+            if ($location->main) {
+                unset($locations[$key]);
+                array_unshift($locations, $location);
+            }
+        }
+
+        return $locations;
+    }
+}
