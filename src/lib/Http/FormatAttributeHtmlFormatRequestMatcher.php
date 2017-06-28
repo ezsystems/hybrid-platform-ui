e71ca07b3e54fccf18935f8e0ca0c0b71c204daa
Bertrand Dunogier <bertrand.dunogier@ez.no>
2017-06-28 17:07:28 +0200

    Autowired event subscribers

---
+++ src/lib/Http/FormatAttributeHtmlFormatRequestMatcher.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace EzSystems\HybridPlatformUi\Http;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestMatcher;
+
+class HtmlFormatRequestMatcher extends RequestMatcher
+{
+    public function __construct()
+    {
+        parent::__construct(null, null, null, null, ['_format' => '^(?!js).*$']);
+    }
+
+    public function match(Request $request)
+    {
+        return parent::matches($request);
+    }
+}
