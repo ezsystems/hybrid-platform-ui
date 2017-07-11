156cac8918e63ef0e669cba5949d74301714fb36
mcurran-inviqa <mcurran@inviqa.com>
2017-07-11 17:11:24 +0100

    Add new location to a content item from the locations tab (#58, EZP-27503)
    * EZP-27503 add location
    
    * EZP-27503 make location field visible
    
    * EZP-27503 add location refactor form actions to private methods
    
    * EZP-27503 fix bug with reset location
    
    * EZP-27503 fix docblock

--- src/bundle/Controller/LocationController.php
+++ src/bundle/Controller/LocationController.php
@@ -8,27 +8,49 @@
  */
 namespace EzSystems\HybridPlatformUiBundle\Controller;
 
+use eZ\Publish\API\Repository\ContentService;
 use eZ\Publish\API\Repository\LocationService;
 use eZ\Publish\API\Repository\Values\Content\Content;
 use eZ\Publish\API\Repository\Values\Content\Location;
 use eZ\Publish\Core\MVC\Symfony\View\ContentView;
 use EzSystems\HybridPlatformUi\Form\UiFormFactory;
 use EzSystems\HybridPlatformUi\Repository\UiLocationService;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouterInterface;
 
 class LocationController extends TabController
 {
-    public function contentViewTabAction(
-        ContentView $view,
+    /**
+     * @var UiLocationService
+     */
+    private $uiLocationService;
+
+    /**
+     * @var UiFormFactory
+     */
+    private $formFactory;
+
+    public function __construct(
         UiLocationService $uiLocationService,
+        RouterInterface $router,
+        ContentService $contentService,
         UiFormFactory $formFactory
+    ) {
+        $this->uiLocationService = $uiLocationService;
+        $this->formFactory = $formFactory;
+        parent::__construct($router, $contentService);
+    }
+
+    public function contentViewTabAction(
+        ContentView $view
     ) {
         $versionInfo = $view->getContent()->getVersionInfo();
         $contentInfo = $versionInfo->getContentInfo();
 
         if ($contentInfo->published) {
-            $locations = $uiLocationService->loadLocations($contentInfo);
-            $actionsForm = $formFactory->createLocationsActionForm($locations);
+            $locations = $this->uiLocationService->loadLocations($contentInfo);
+            $actionsForm = $this->formFactory->createLocationsActionForm($locations);
 
             $view->addParameters([
                 'locations' => $locations,
@@ -41,25 +63,21 @@ class LocationController extends TabController
 
     public function actionsAction(
         Content $content,
-        Request $request,
-        UiLocationService $uiLocationService,
-        UiFormFactory $formFactory
+        Request $request
     ) {
-        $actionsForm = $formFactory->createLocationsActionForm();
+        $actionsForm = $this->formFactory->createLocationsActionForm();
         $actionsForm->handleRequest($request);
 
         $redirectLocationId = $request->query->get('redirectLocationId', $content->contentInfo->mainLocationId);
 
         if ($actionsForm->isValid()) {
-            $locationIds = array_keys($actionsForm->get('removeLocations')->getData());
+            $resetLocation = $this->deleteLocationsBasedOnFormSubmit($actionsForm, $redirectLocationId);
 
-            if ($actionsForm->get('delete')->isClicked()) {
-                $uiLocationService->deleteLocations($locationIds);
-
-                if (in_array($redirectLocationId, $locationIds)) {
-                    return $this->resetLocation($content->id);
-                }
+            if ($resetLocation) {
+                return $this->resetLocation($content->id);
             }
+
+            $this->addLocationBasedOnFormSubmit($actionsForm, $content);
         }
 
         return $this->reloadTab('locations', $content->id, $redirectLocationId);
@@ -80,5 +98,38 @@ class LocationController extends TabController
         }
 
         return $this->reloadTab('details', $content->id, $location->id);
+    }
+
+    /**
+     * Delete locations based on form submit.
+     *
+     * @param FormInterface $form
+     * @param mixed $redirectLocationId
+     *
+     * @return bool Whether to reset location or not.
+     */
+    private function deleteLocationsBasedOnFormSubmit(FormInterface $form, $redirectLocationId)
+    {
+        $locationIds = array_keys($form->get('removeLocations')->getData());
+
+        if ($form->get('delete')->isClicked()) {
+            $this->uiLocationService->deleteLocations($locationIds);
+
+            if (in_array($redirectLocationId, $locationIds)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function addLocationBasedOnFormSubmit(FormInterface $form, Content $content)
+    {
+        if ($form->get('add')->isClicked()) {
+            $this->uiLocationService->addLocation(
+                $content->contentInfo,
+                $form->get('parentLocationId')->getData()
+            );
+        }
     }
 }
