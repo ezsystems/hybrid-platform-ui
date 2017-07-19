1c0de26a601047072f6cf9f049639a5a66628aa8
Bertrand Dunogier <bertrand.dunogier@ez.no>
2017-07-20 01:27:30 +0200

    Refactored app rendering
    The AppRenderer is removed. The App's rendering methods are used instead.

--- spec/EventSubscriber/AppRendererSubscriberSpec.php
+++ spec/EventSubscriber/AppRendererSubscriberSpec.php
@@ -2,16 +2,18 @@
 
 namespace spec\EzSystems\HybridPlatformUi\EventSubscriber;
 
-use EzSystems\HybridPlatformUi\App\AppResponseRenderer;
 use EzSystems\HybridPlatformUi\App\ToolbarsConfigurator;
 use EzSystems\HybridPlatformUi\Components\App;
 use EzSystems\HybridPlatformUi\EventSubscriber\AppRendererSubscriber;
+use EzSystems\HybridPlatformUi\Http\AjaxUpdateRequestMatcher;
 use EzSystems\HybridPlatformUi\Http\HybridRequestMatcher;
 use EzSystems\HybridPlatformUi\Http\Response\NotificationResponse;
 use EzSystems\HybridPlatformUi\Http\Response\ResetResponse;
+use PhpParser\Node\Arg;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\ParameterBag;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -27,12 +29,12 @@ class AppRendererSubscriberSpec extends ObjectBehavior
 {
     function let(
         App $app,
-        AppResponseRenderer $renderer,
         GetResponseForControllerResultEvent $event,
         Request $request,
         Response $response,
         ParameterBag $requestAttributes,
         HybridRequestMatcher $hybridRequestMatcher,
+        AjaxUpdateRequestMatcher $ajaxUpdateRequestMatcher,
         ToolbarsConfigurator $toolbarsConfigurator
     ) {
         $hybridRequestMatcher->matches(Argument::type(Request::class))->willReturn(true);
@@ -44,7 +46,12 @@ class AppRendererSubscriberSpec extends ObjectBehavior
         $event->isMasterRequest()->willReturn(true);
         $response->isRedirect()->willReturn(false);
 
-        $this->beConstructedWith($app, $renderer, $hybridRequestMatcher, $toolbarsConfigurator);
+        $this->beConstructedWith(
+            $app,
+            $hybridRequestMatcher,
+            $ajaxUpdateRequestMatcher,
+            $toolbarsConfigurator
+        );
     }
 
     function it_is_initializable()
@@ -60,14 +67,12 @@ class AppRendererSubscriberSpec extends ObjectBehavior
     }
 
     function it_ignores_non_admin_requests(
-        AppResponseRenderer $renderer,
         FilterResponseEvent $event,
         Request $request,
-        HybridRequestMatcher $hybridRequestMatcher,
-        Response $response
+        HybridRequestMatcher $hybridRequestMatcher
     ) {
         $hybridRequestMatcher->matches($request)->willReturn(false);
-        $renderer->render($response)->shouldNotBeCalled();
+        $event->setResponse(Argument::type(Response::class))->shouldNotBeCalled();
 
         $this->renderApp($event);
     }
@@ -76,50 +81,73 @@ class AppRendererSubscriberSpec extends ObjectBehavior
     {
         $event->isMasterRequest()->willReturn(false);
         $event->getRequest()->shouldNotBeCalled();
+
         $this->renderApp($event);
     }
 
     function it_ignores_redirect_responses(
         FilterResponseEvent $event,
-        AppResponseRenderer $renderer,
         Response $response
     ) {
         $response->isRedirect()->willReturn(true);
-        $renderer->render(Argument::any())->shouldNotBeCalled();
+        $event->setResponse(Argument::type(Response::class))->shouldNotBeCalled();
 
         $this->renderApp($event);
     }
 
     function it_ignores_reset_responses(
         FilterResponseEvent $event,
-        AppResponseRenderer $renderer,
         ResetResponse $resetResponse
     ) {
         $event->getResponse()->willReturn($resetResponse);
-        $renderer->render(Argument::any())->shouldNotBeCalled();
+        $event->setResponse(Argument::type(Response::class))->shouldNotBeCalled();
 
         $this->renderApp($event);
     }
 
     function it_ignores_notification_responses(
         FilterResponseEvent $event,
-        AppResponseRenderer $renderer,
         NotificationResponse $notificationResponse
     ) {
         $event->getResponse()->willReturn($notificationResponse);
-        $renderer->render(Argument::any())->shouldNotBeCalled();
+        $event->setResponse(Argument::type(Response::class))->shouldNotBeCalled();
 
         $this->renderApp($event);
     }
 
     function it_configures_the_toolbars_and_renders_the_app(
         App $app,
-        AppResponseRenderer $renderer,
         FilterResponseEvent $event,
         ToolbarsConfigurator $toolbarsConfigurator
     ) {
-        $renderer->render(Argument::type(Response::class), $app)->shouldBeCalled();
         $toolbarsConfigurator->configureToolbars($app)->shouldBeCalled();
+        $event->setResponse(Argument::type(Response::class))->shouldBeCalled();
+
+        $this->renderApp($event);
+    }
+
+    function it_renders_ajax_update_requests_to_json(
+        AjaxUpdateRequestMatcher $ajaxUpdateRequestMatcher,
+        App $app,
+        FilterResponseEvent $event,
+        Request $request
+    ) {
+        $ajaxUpdateRequestMatcher->matches($request)->shouldBeCalled()->willReturn(true);
+        $app->jsonSerialize()->shouldBeCalled()->willReturn("update json");
+        $event->setResponse(Argument::type(JsonResponse::class))->shouldBeCalled();
+
+        $this->renderApp($event);
+    }
+
+    function it_renders_hybrid_requests_to_html(
+        AjaxUpdateRequestMatcher $ajaxUpdateRequestMatcher,
+        App $app,
+        FilterResponseEvent $event,
+        Request $request
+    ) {
+        $ajaxUpdateRequestMatcher->matches($request)->shouldBeCalled()->willReturn(false);
+        $app->renderToString()->shouldBeCalled()->willReturn("<html />");
+        $event->setResponse(Argument::type(Response::class))->shouldBeCalled();
 
         $this->renderApp($event);
     }
--- src/lib/EventSubscriber/AppRendererSubscriber.php
+++ src/lib/EventSubscriber/AppRendererSubscriber.php
@@ -2,12 +2,15 @@
 
 namespace EzSystems\HybridPlatformUi\EventSubscriber;
 
-use EzSystems\HybridPlatformUi\App\AppResponseRenderer;
 use EzSystems\HybridPlatformUi\App\ToolbarsConfigurator;
 use EzSystems\HybridPlatformUi\Components\App;
+use EzSystems\HybridPlatformUi\Http\AjaxUpdateRequestMatcher;
 use EzSystems\HybridPlatformUi\Http\HybridRequestMatcher;
 use EzSystems\HybridPlatformUi\Http\Response\NoRenderResponse;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
@@ -24,24 +27,29 @@ class AppRendererSubscriber implements EventSubscriberInterface
     private $app;
 
     /**
-     * @var \EzSystems\HybridPlatformUi\App\AppResponseRenderer
-     */
-    private $appRenderer;
-
-    /**
      * @var \EzSystems\HybridPlatformUi\App\ToolbarsConfigurator
      */
     private $toolbarsConfigurator;
 
+    /**
+     * @var \EzSystems\HybridPlatformUi\Http\AjaxUpdateRequestMatcher
+     */
+    private $ajaxUpdateRequestMatcher;
+
+    /**
+     * @var \EzSystems\HybridPlatformUi\EventSubscriber\PartialHtmlRequestMatcher
+     */
+    private $partialHtmlRequestMatcher;
+
     public function __construct(
         App $app,
-        AppResponseRenderer $appRenderer,
         HybridRequestMatcher $hybridRequestMatcher,
+        AjaxUpdateRequestMatcher $ajaxUpdateRequestMatcher,
         ToolbarsConfigurator $toolbarsConfigurator
     ) {
-        $this->hybridRequestMatcher = $hybridRequestMatcher;
         $this->app = $app;
-        $this->appRenderer = $appRenderer;
+        $this->hybridRequestMatcher = $hybridRequestMatcher;
+        $this->ajaxUpdateRequestMatcher = $ajaxUpdateRequestMatcher;
         $this->toolbarsConfigurator = $toolbarsConfigurator;
     }
 
@@ -71,6 +79,25 @@ class AppRendererSubscriber implements EventSubscriberInterface
         }
 
         $this->toolbarsConfigurator->configureToolbars($this->app);
-        $this->appRenderer->render($response, $this->app);
+
+        $event->setResponse($this->renderAppResponse($request));
+    }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    private function renderAppResponse(Request $request)
+    {
+        if ($this->ajaxUpdateRequestMatcher->matches($request)) {
+            $response = new JsonResponse($this->app);
+        } else {
+            $response = new Response(
+                $this->app->renderToString()
+            );
+        }
+
+        return $response;
     }
 }
